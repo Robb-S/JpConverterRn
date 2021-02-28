c0cac2136a5a7cc4cd909c37cd0c0cbf4481250e
Robb S <rsreg@bento.com>
2021-02-28 21:49:50 +0900

    Start help screen

--- src/components/HelpScreen.js
+++ src/components/HelpScreen.js
@@ -1,8 +1,36 @@
 import React from 'react';
 import { Text, StyleSheet, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 import SwipeGesture from '../utils/swipe-gesture2'
 import { clr } from '../utils/colors';
 import NarrowBtn from './NarrowBtn';
+import {HelpHtml3} from './helphtml.js';
+const HelpHtml = require('../html/help.html');
+
+const HelpHtml2 = 
+`
+<html><head>
+<meta name="HandHeldFriendly" content="true" />
+<meta name="viewport"  content="width=device-width,initial-scale=1.0,user-scalable=yes" />
+</head><body>
+
+<div class="note">
+
+<h3>General navigation</h3>
+<p>
+Swipe the screen right or left to switch between different types of conversion, or use the navigation drawer by swiping from the left edge or pressing the menu (three bars) button on the upper left.
+</p>
+
+<h3>Metric conversion</h3>
+<p>
+Press the "CONVERT TO METRIC" text to toggle back and forth between conversion modes, or use the left-hand navigation drawer.
+</p><p>
+The metric calculator converts between several common metric units (meters, kilograms, degrees centigrade, etc.) and their corresponding imperial units.  (Fluid ounces are US units.)  Choose the units you wish to convert from the drop-down list.
+</p>
+<br />
+</div>
+</body></html>
+`
 
 const HelpScreen = ({navigation}) => {
 
@@ -10,20 +38,25 @@ const HelpScreen = ({navigation}) => {
   const onSwipePerformed = (action) => {    
     if (action==='left') {navigation.goBack()}
   }
-
+  // console.log('typeof ' + typeof(HelpHtml) + ' ' + HelpHtml);
   return (
-    <View style={styles.container} >
-    <SwipeGesture gestureStyle={styles.swipeGestureContainer} 
-      onSwipePerformed={onSwipePerformed}>
-        <View style={styles.innerView}>
-          <Text style={styles.mainText}>Please hlep me!</Text>
-          <NarrowBtn 
-            onPress={() => navigation.goBack()}
-            text={'Go back'} bgColor={clr.lightGrey} color={clr.darkGrey} />
-          <Text style={styles.mainText}>More text....</Text>
-        </View>
-      </SwipeGesture>
-    </View>
+    <WebView 
+    originWhitelist={['*']}
+    source={ {html: HelpHtml3 } }
+    />
+
+    // <View style={styles.container} >
+    // <SwipeGesture gestureStyle={styles.swipeGestureContainer} 
+    //   onSwipePerformed={onSwipePerformed}>
+    //     <View style={styles.innerView}>
+    //       <Text style={styles.mainText}>Please hlep me!</Text>
+    //       <NarrowBtn 
+    //         onPress={() => navigation.goBack()}
+    //         text={'Go back'} bgColor={clr.lightGrey} color={clr.darkGrey} />
+    //       <Text style={styles.mainText}>More text....</Text>
+    //     </View>
+    //   </SwipeGesture>
+    // </View>
   )
 }
 
---
+++ src/components/helphtml.js
@@ -0,0 +1,77 @@
+
+export const HelpHtml3 =
+`
+<html><head>
+<meta name="HandHeldFriendly" content="true" />
+<meta name="viewport"  content="width=device-width,initial-scale=1.0,user-scalable=yes" />
+<style>
+body {
+    margin: 0;
+    padding: 4px;
+    background-color: #ffffff; /* Background color */
+    background-color: #000000; /* Background color */
+    color: #333333;            /* Foreground color used for text */
+    color: #e9e9e9;            /* Foreground color used for text */
+    font: 17px/1.3 Helvetica;
+}
+h3 {
+    background-color: #2e7d32;  /* darker green */
+    padding: 8px 5px 8px 12px;
+}
+div.note {
+    padding: 0 15px 0 0;
+    margin: 12px 5px 35px 12px;
+}   
+div.note p {
+    margin-bottom: 15px;
+}
+</style>
+</head><body>
+
+<div class="note">
+
+<h3>General navigation</h3>
+<p>
+Swipe the screen right or left to switch between different types of conversion, or use the navigation drawer by swiping from the left edge.
+</p>
+
+</div><div class="note">
+<h3>Metric conversion</h3>
+<p>
+Press the "CONVERT TO METRIC" text to toggle back and forth between conversion modes, or use the left-hand navigation drawer.
+</p><p>
+The metric calculator converts between several common metric units (meters, kilograms, degrees centigrade, etc.) and their corresponding imperial units.  (Fluid ounces are US units.)  Choose the units you wish to convert from the drop-down list.
+</p>
+
+</div><div class="note">
+<h3>Metric conversion</h3>
+<p>
+Press the "CONVERT TO METRIC" text to toggle back and forth between conversion modes, or use the left-hand navigation drawer.
+</p><p>
+The metric calculator converts between several common metric units (meters, kilograms, degrees centigrade, etc.) and their corresponding imperial units.  (Fluid ounces are US units.)  Choose the units you wish to convert from the drop-down list.
+</p>
+</div><div class="note">
+<h3>Metric conversion</h3>
+<p>
+Press the "CONVERT TO METRIC" text to toggle back and forth between conversion modes, or use the left-hand navigation drawer.
+</p><p>
+The metric calculator converts between several common metric units (meters, kilograms, degrees centigrade, etc.) and their corresponding imperial units.  (Fluid ounces are US units.)  Choose the units you wish to convert from the drop-down list.
+</p>
+</div><div class="note">
+<h3>Metric conversion</h3>
+<p>
+Press the "CONVERT TO METRIC" text to toggle back and forth between conversion modes, or use the left-hand navigation drawer.
+</p><p>
+The metric calculator converts between several common metric units (meters, kilograms, degrees centigrade, etc.) and their corresponding imperial units.  (Fluid ounces are US units.)  Choose the units you wish to convert from the drop-down list.
+</p>
+</div><div class="note">
+<h3>Metric conversion</h3>
+<p>
+Press the "CONVERT TO METRIC" text to toggle back and forth between conversion modes, or use the left-hand navigation drawer.
+</p><p>
+The metric calculator converts between several common metric units (meters, kilograms, degrees centigrade, etc.) and their corresponding imperial units.  (Fluid ounces are US units.)  Choose the units you wish to convert from the drop-down list.
+</p>
+
+</div>
+</body></html>
+`
